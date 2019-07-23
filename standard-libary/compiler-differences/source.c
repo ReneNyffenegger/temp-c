b95b0b772ea8d4443ae400f24065ef7fb8191dfd
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-23 11:22:31 +0200

    wcscmp

--- standard-libary/compiler-differences/source.c
+++ standard-libary/compiler-differences/source.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+#include <wchar.h>
 
 int main() {
 
+
 #ifdef _MSC_VER
-  //
-  //    Visual Studio compiler (cl) has strcmp rather than strcasecmp
-  //
-#define strcasecmp stricmp
+    printf("defined: _MSC_VER\n");
+ //
+ //    Visual Studio compiler (cl) has stricmp rather than strcasecmp
+ //
+    #define strcasecmp stricmp
+    #define wcscasecmp wcsicmp
+
 #endif
 
-    printf("strcasecmp = %d\n", strcasecmp("foo", "FOO"));
+
+#ifdef __MINGW32__
+    printf("defined: __MINGW32__\n");
+
+    #define wcscasecmp wcsicmp // or _wcsicmp ?
+
+#endif
+
+
+#ifdef __GNUC__
+    printf("defined: __GNUC__\n");
+#endif
+
+
+    printf("strcasecmp = %d\n", strcasecmp( "foo",  "FOO"));
+    printf("wcscasecmp = %d\n", wcscasecmp(L"foo", L"FOO"));
 
 }
