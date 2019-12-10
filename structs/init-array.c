915ddb611540128146c3ff9b1d87fa94f6ee2b40
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-12-10 08:41:56 +0100

    n/a

--- structs/init-array.c
+++ structs/init-array.c
@@ -1,3 +1,6 @@
+//
+//  gcc -O0 init-array.c && a
+//
 #include <stdio.h>
 
 enum E {
@@ -16,14 +19,40 @@ typedef struct S {
 
 static S *translations[NUM_NUMBERS] = {NULL, NULL};
 
+
+void f() {
+
+   long i = 42;
+   long j = 43;
+   long k = 44;
+   long l = 45;
+
+}
+void g() {
+
+   for (int i=0; i<NUM_NUMBERS; i++) {
+      printf("%x\n", translations[i]);
+   }
+
+   S *local[NUM_NUMBERS];
+   for (int i=0; i<NUM_NUMBERS; i++) {
+      printf("%x\n", local[i]);
+   }
+
+   S *another_local[NUM_NUMBERS] = {0};
+   for (int i=0; i<NUM_NUMBERS; i++) {
+      printf("%x\n", another_local[i]);
+   }
+
+}
+
 int main() {
+
 
    printf("NUM_NUMBERS = %d\n", NUM_NUMBERS); // 4
 
-   for (int i=0; i<NUM_NUMBERS; i++) {
+   f();
+   g();
 
-      printf("%x\n", translations[i]);
-
-   }
 
 }
